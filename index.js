2f9a619759850a04e37576f3adb3d0004f76b2bb
Finley Mwangola <finley.mwachia12@gmail.com>
2024-10-20 10:00:23 +0300

    add the code that captures  data from the users and stores in it a message.txt file

--- index.js
+++ index.js
@@ -3,3 +3,27 @@
 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
 3. Create a txt file to save the user input using the native fs node module.
 */
+import inquirer from "inquirer";
+import { writeFile } from 'node:fs';
+import { Buffer } from 'node:buffer';
+
+inquirer
+  .prompt([
+   {
+    type:"input",
+    name :"links",
+    message:"Which link do you want to convert to a qr code ?"
+   }
+  ])
+  .then((answers) => {
+    console.log(answers.links)
+    const data = new Uint8Array(Buffer.from(answers.links));
+    writeFile('message.txt', data, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      }); 
+  });
+
+  
+  
+  
